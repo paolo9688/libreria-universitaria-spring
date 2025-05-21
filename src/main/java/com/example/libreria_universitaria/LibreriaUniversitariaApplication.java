162766355137529468ef9a2837a7d65397479a54
paolo9688 <paolo.pz1@gmail.com>
2025-05-21 10:59:21 +0200

    first commit

---
+++ src/main/java/com/example/libreria_universitaria/LibreriaUniversitariaApplication.java
@@ -0,0 +1,13 @@
+package com.example.libreria_universitaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaUniversitariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriaUniversitariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/libreria_universitaria/LibreriaUniversitariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libreria_universitaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriaUniversitariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
